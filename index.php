0ee726cfb5af46dae7186b7e5fe8af5236a27164
Paiaro <gabrielpaiarop@gmail.com>
2024-06-20 21:42:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar com Busca - Bootstrap</title>
+    <title>EasyShop</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
@@ -56,13 +56,26 @@
             border-radius: 50%;
             margin-bottom: 10px;
         }
+
+        /* Centralizar conteúdo principal */
+        .container {
+            text-align: center;
+        }
+
+        /* Ajustar espaçamento para centralizar na vertical */
+        .banner,
+        .mainBanner {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+    </style>
     </style>
 </head>
 
 <body>
-
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Sua Loja</a>
+        <a class="navbar-brand" href="#">EasyShop</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -92,77 +105,75 @@
             </form>
         </div>
     </nav>
-
-    <div class="container-fluid banner">
-        <div class="row">
-            <div class="col-12">
-                <h1>Melhores preços</h1>
-                <p>Preços incríveis em todos os seus itens favoritos</p>
-                <p>Obtenha mais por menos em marcas selecionadas</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid mt-3">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="mainBanner" style="background-image: url('imagens/celular-banner.jpg');">
-                    <h2>Ofertas de férias</h2>
-                    <p> Marcas de smartphones selecionadas</p>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-primary">Comprar</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="mainBanner" style="background-image: url('imagens/fones-banner.jpg');">
-                    <h2>Comfira já</h2>
-                    <p> Leve o seu som em qualquer lugar</p>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-primary">Comprar</button>
-
-                    </div>
+    <div class="container">
+        <div class="container-fluid banner">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Melhores preços</h1>
+                    <p>Preços incríveis em todos os seus itens favoritos</p>
+                    <p>Obtenha mais por menos em marcas selecionadas</p>
                 </div>
             </div>
         </div>
-    </div>
 
-    <h2 class="mt-5">Categorias</h2>
+        <div class="container-fluid mt-3">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="mainBanner" style="background-image: url('imagens/celular-banner.jpg');">
+                        <h2>Ofertas de férias</h2>
+                        <p> Marcas de smartphones selecionadas</p>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary">Comprar</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="mainBanner" style="background-image: url('imagens/fones-banner.jpg');">
+                        <h2>Comfira já</h2>
+                        <p> Leve o seu som em qualquer lugar</p>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary">Comprar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <div class="row mt-3">
-        <div class="col-md-2">
-            <div class="categoria">
-                <img src="imagens/pc-categoria.jpg" alt="Computadores">
-                <h3>Computadores</h3>
+        <h2 class="mt-5">Categorias</h2>
+
+        <div class="row mt-3 justify-content-center">
+            <div class="col-md-2">
+                <div class="categoria">
+                    <img src="imagens/pc-categoria.jpg" alt="Computadores">
+                    <h3>Pcs</h3>
+                </div>
             </div>
-        </div>
-        <div class="col-md-2">
-            <div class="categoria">
-                <img src="imagens/smartphone-categoria.jpg" alt="Celulares">
-                <h3>Celulares</h3>
+            <div class="col-md-2">
+                <div class="categoria">
+                    <img src="imagens/smartphone-categoria.jpg" alt="Celulares">
+                    <h3>Celulares</h3>
+                </div>
             </div>
-        </div>
-        <div class="col-md-2">
-            <div class="categoria">
-                <img src="imagens/tablet.jpg" alt="Tablets">
-                <h3>Tablets</h3>
+            <div class="col-md-2">
+                <div class="categoria">
+                    <img src="imagens/tablet-categoria.jpg" alt="Tablets">
+                    <h3>Tablets</h3>
+                </div>
             </div>
-        </div>
-        <div class="col-md-2">
-            <div class="categoria">
-                <img src="imagens/fone.jpg" alt="Fones">
-                <h3>Fones</h3>
+            <div class="col-md-2">
+                <div class="categoria">
+                    <img src="imagens/headphones-categoria.jpg" alt="Fones">
+                    <h3>Fones</h3>
+                </div>
             </div>
-        </div>
-        <div class="col-md-2">
-            <div class="categoria">
-                <img src="imagens/tv.jpg" alt="TVs">
-                <h3>TVs</h3>
+            <div class="col-md-2">
+                <div class="categoria">
+                    <img src="imagens/tvs-categoria.jpg" alt="TVs">
+                    <h3>TVs</h3>
+                </div>
             </div>
         </div>
     </div>
-    </div>
-
 </body>
 
 </html>
