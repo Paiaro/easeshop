93ea3098bd5d1b8b58e68a8c46b4920893adc7e5
Paiaro <gabrielpaiarop@gmail.com>
2024-06-21 21:46:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Todos Produtos
+                       Produtos
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <a class="dropdown-item" href="#">Computadores</a>
