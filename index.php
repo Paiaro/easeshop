1f4f00b804ed0ee0531b992d2646e6ccae2017ef
Paiaro <gabrielpaiarop@gmail.com>
2024-06-21 21:44:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,21 @@
             flex-direction: column;
             justify-content: center;
         }
-    </style>
+
+        .footer {
+            background-color: #343a40;
+            color: white;
+            padding: 40px 0;
+        }
+
+        .footer a {
+            color: #f8f9fa;
+            text-decoration: none;
+        }
+
+        .footer a:hover {
+            text-decoration: underline;
+        }
     </style>
 </head>
 
@@ -172,6 +186,37 @@
             </div>
         </div>
     </div>
+
+    <footer class="footer mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h5>Sobre Nós</h5>
+                    <p>EasyShop é a sua loja online de confiança para todos os produtos eletrônicos. Oferecemos as melhores ofertas e serviços.</p>
+                </div>
+                <div class="col-md-4">
+                    <h5>Links Úteis</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Início</a></li>
+                        <li><a href="#">Cadastre-se</a></li>
+                        <li><a href="#">Produtos</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h5>Contato</h5>
+                    <ul class="list-unstyled">
+                        <li>Email: contato@easyshop.com</li>
+                        <li>Telefone: (11) 1234-5678</li>
+                        <li>Endereço: Rua Exemplo, 123, São Paulo - SP</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="text-center mt-3">
+                <p>&copy; 2024 EasyShop. Todos os direitos reservados.</p>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
