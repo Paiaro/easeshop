0afc61cd6aa775231e937129ff8f9bc5bd6062a9
Paiaro <gabrielpaiarop@gmail.com>
2024-06-24 16:01:19 -0300

    .

--- inc/cabecalho-admin.php
+++ inc/cabecalho-admin.php
@@ -40,7 +40,7 @@ function ocultarGerenciador()
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="../index.php">Home <span class="sr-only"></span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Cadastre-se</a>
