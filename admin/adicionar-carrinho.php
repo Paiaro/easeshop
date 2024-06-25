76e80b349aaacd105b11824dcca0cb0f754551ef
Paiaro <gabrielpaiarop@gmail.com>
2024-06-25 17:28:47 -0300

    Update adicionar-carrinho.php

--- admin/adicionar-carrinho.php
+++ admin/adicionar-carrinho.php
@@ -1,16 +1,20 @@
 <?php
-require_once "config.php";
+require_once "../inc/cabecalho.php"; 
+require_once "../inc/conecta.php"; 
+require_once "../inc/funcoes-usuarios.php"; 
+require_once "../inc/funcoes-sessao.php"; 
+
 
 if (isset($_POST['produto_id'])) {
     $produtoId = $_POST['produto_id'];
 
-    // Obter detalhes do produto do banco de dados (ou de onde você os armazena)
+  
     $sql = "SELECT * FROM produtos WHERE id = $produtoId";
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
     $produto = mysqli_fetch_assoc($resultado);
 
     if ($produto) {
-        // Adicionar o produto ao carrinho
+        
         if (isset($_SESSION['carrinho'][$produtoId])) {
             $_SESSION['carrinho'][$produtoId]['quantidade']++;
         } else {
@@ -23,7 +27,7 @@ if (isset($_POST['produto_id'])) {
     }
 }
 
-// Redirecionar para a página do carrinho
+
 header('Location: carrinho.php');
 exit;
 ?>
