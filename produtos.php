cd1ba706561b07f3d9b6eb817af3c5f28572ad75
Paiaro <gabrielpaiarop@gmail.com>
2024-06-25 19:49:00 -0300

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -7,25 +7,24 @@ $listaProdutos = lerProdutos($conexao);
 
 <style>
     .card {
-        max-width: 20rem; 
+        max-width: 20rem;
     }
 </style>
 <link rel="stylesheet" href="css/estilo-index.css">
 <div class="row my-1 mx-md-n1">
-    <?php foreach ($listaProdutos as $produto): ?>
+    <?php foreach ($listaProdutos as $produto) : ?>
         <div class="col-md-2 my-1 px-md-1">
             <article class="card shadow-sm h-100">
-                <a href="comprar.php?id=<?=$produto['id']?>" class="card-link">
-                    <img src="imagens/<?=$produto['imagem']?>" class="card-img-top" alt="">
+                <a href="comprar.php?id=<?= $produto['id'] ?>" class="card-link">
+                    <img src="imagens/<?= $produto['imagem'] ?>" class="card-img-top" alt="">
                 </a>
                 <div class="card-body">
-                    <h3 class="fs-4 card-title"><?=$produto['nome']?></h3>
+                    <h3 class="fs-4 card-title"><?= $produto['nome'] ?></h3>
                     <p class="card-text">
                         R$ <?= number_format($produto['preco'], 2, ',', '.') ?>
                     </p>
-                    <a href="comprar.php?id=<?=$produto['id']?>" class="btn btn-primary">Comprar</a>
-                    
-                    <form action="adicionar-carrinho.php" method="post" style="display: inline;"> 
+
+                    <form action="admin/adicionar-carrinho.php" method="post" style="display: inline;">
                         <input type="hidden" name="produto_id" value="<?= $produto['id'] ?>">
                         <button type="submit" class="btn btn-success">Adicionar ao Carrinho</button>
                     </form>
