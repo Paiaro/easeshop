1b93debd03546c932a2ce85a42c7ac5099f8b4bc
Paiaro <gabrielpaiarop@gmail.com>
2024-06-26 14:30:46 -0300

    Update database_integrador.sql

--- database_integrador.sql
+++ database_integrador.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2024 at 12:38 PM
+-- Generation Time: Jun 26, 2024 at 07:30 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,12 +41,14 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `preco`, `imagem`) VALUES
-(1, 'Notebook Acer Nitro V15 ANV15-51-58AZ 13ª Geração Intel Core i5-13420H, 8GB RAM, 512GB SSD, NVIDIA R', 5.00, '../uploads/51Wv-tEUn6L._AC_SL1000_.jpg'),
-(2, 'Notebook Acer Nitro V15 ANV15-51-58AZ 13ª Geração Intel Core i5-13420H, 8GB RAM, 512GB SSD, NVIDIA R', 5.00, '../uploads/51Wv-tEUn6L._AC_SL1000_.jpg'),
-(5, 'Notebook Acer Nitro V15', 2.00, '../uploads/51Wv-tEUn6L._AC_SL1000_.jpg'),
-(6, 'Notebook Acer Aspire 3', 2.00, '../uploads/512JJoQh4lL.__AC_SX300_SY300_QL70_ML2_.jpg'),
-(7, 'Notebook Acer Aspire 3', 2.00, '../uploads/512JJoQh4lL.__AC_SX300_SY300_QL70_ML2_.jpg'),
-(8, 'Notebook Gamer ASUS TUF F15 Intel Core i7 12700h', 5000.00, '../uploads/71zxWLbeYYL.__AC_SX300_SY300_QL70_ML2_.jpg');
+(10, 'Havit HV-H2232d - Fone de Ouvido', 114.90, '../uploads/Havit HV-H2232d.jpg'),
+(11, 'Headphone Fone de Ouvido Havit HV-H2002d', 169.90, '../uploads/71sV-QnV8TL._AC_UY218_.jpg'),
+(13, 'Computador Gamer Pc Completo Intel Core i5', 180.12, '../uploads/Computador Gamer Pc Intel Core i5.jpg'),
+(14, 'Pc Gamer Cpu Ryzen 7 8700G / SSD 2TB M.2 NVMe', 9869.00, '../uploads/51mY3KbAY3L._AC_UL320_.jpg'),
+(15, 'PC GAMER AMD RYZEN 5 4600G - 16GB DDR4 - NVME 256GB - RADEON VEGA 7', 2390.00, '../uploads/613fZQDMveL._AC_UL320_.jpg'),
+(16, 'Notebook Gamer ASUS TUF F15 Intel Core i7 12700h 2,3 GHz 8GB RAM 512GB SSD Linux KeepOS NVidia GeFor', 5804.00, '../uploads/7183e2xk8iL._AC_UL320_.jpg'),
+(17, 'Notebook Gamer ROG Strix G16, NVidia RTX4060, CORE I9, 16 GB, 512 GB', 11499.00, '../uploads/51O4bS147tL._AC_UL320_.jpg'),
+(18, 'Notebook Gamer Legion Slim 5 Intel Core i5-13420H 16GB 512GB RTX 3050 6GB', 5999.00, '../uploads/71sqxJweoYL._AC_UL320_.jpg');
 
 -- --------------------------------------------------------
 
@@ -93,7 +95,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
