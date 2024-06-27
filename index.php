dd25a8ea711df4f064d473d11175fb633df01067
Paiaro <gabrielpaiarop@gmail.com>
2024-06-27 14:31:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -123,10 +123,6 @@ require_once "inc/cabecalho.php";
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
 <?php 
 require_once "inc/footer.php"; 
 ?>
